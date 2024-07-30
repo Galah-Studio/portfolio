1de12246e9b89d81bab94cc8c059cb25634496f5
Galah-Studio <hamid@galah.tv>
2024-07-30 15:55:54 +0300

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,4 +1,2 @@
-import { LINKS, EXTRA_LINKS } from "@/constants";
-
-export type SectionName = (typeof LINKS)[number]["name"];
-export type ExtraLinks = keyof typeof EXTRA_LINKS;
+import { LINKS } from "@/constants";
+import { EXTRA_LINKS } from "@/constants";

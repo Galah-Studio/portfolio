175575d7f1a399fecaa339b4aee8039e257e10ae
Galah-Studio <hamid@galah.tv>
2024-07-31 00:05:22 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,12 +21,10 @@ export const viewport: Viewport = {
 // site metadata
 export const metadata: Metadata = {
   title: `${OWNER_NAME.split(" ")[0]} | Personal Portfolio`,
-  description: `${
-    OWNER_NAME.split(" ")[0]
-  } is a full-stack developer with 4 years of experience.`,
+  description: `${OWNER_NAME.split(" ")[0]} is a full-stack developer with 4 years of experience.`,
   authors: {
     name: OWNER_NAME,
-    url: EXTRA_LINKS.Github, // Corrected here
+    url: "https://www.google.com/",
   },
   keywords: [
     "reactjs",
@@ -51,22 +49,15 @@ export const metadata: Metadata = {
   ],
   manifest: "/manifest.json",
   other: {
-    "google-site-verification":
-      process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION_KEY!,
+    "google-site-verification": process.env.NEXT_PUBLIC_GOOGLE_VERIFICATION_KEY!,
   },
 };
 
 // root layout
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body
-        className={`${inter.className} bg-gray-50 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 text-gray-950 relative pt-28 sm:pt-36`}
-      >
+      <body className={`${inter.className} bg-gray-50 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 text-gray-950 relative pt-28 sm:pt-36`}>
         {/* bg blur effect */}
         <div className="bg-[#fbe2e3] dark:bg-[#946263] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]" />
         <div className="bg-[#dbd7fb] dark:bg-[#676394] absolute top-[-1rem] -z-10 left-[-35rem] h-[50rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
@@ -83,12 +74,7 @@ export default function RootLayout({
 
             {/* toaster */}
             <aside>
-              <Toaster
-                position="top-right"
-                toastOptions={{
-                  className: "dark:bg-white/10 dark:text-white/80",
-                }}
-              />
+              <Toaster position="top-right" toastOptions={{ className: "dark:bg-white/10 dark:text-white/80" }} />
             </aside>
 
             {/* theme switcher */}
